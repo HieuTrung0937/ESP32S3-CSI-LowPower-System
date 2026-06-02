41b43b813c0f714a2169d0ffbf9278d8e13a413c
HieuTrung0937 <ngotsacombank@gmail.com>
2026-06-02 21:06:41 +0700

    cau truc thu vien

--- esp32s3_csi_rx/src/main.cpp
+++ esp32s3_csi_rx/src/main.cpp
@@ -1,6 +1,22 @@
-#include "CsiReceiver.hpp"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_timer.h>
+#include <cstdio>
+#include <CSIFallGuard.h>
+// MAC thật của 2 board
+uint8_t my_tx_mac[] = {0x80, 0xB5, 0x4E, 0xDE, 0xEE, 0x70};
+uint8_t my_rx_mac[] = {0x80, 0xB5, 0x4E, 0xDE, 0xEC, 0x48};
 
 extern "C" void app_main() {
-    static CsiReceiver rx;
-    rx.begin();
+    // Cấu hình MAC trước khi begin
+    CSIFallGuard_set_tx_mac(my_tx_mac);
+    CSIFallGuard_set_rx_mac(my_rx_mac);
+    CSIFallGuard_set_channel(1);
+    
+    CSIFallGuard_begin(MODE_RX, 1);
+    
+    while (1) {
+        CSIFallGuard_update();
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
 }
--- esp32s3_csi_tx/src/main.cpp
+++ esp32s3_csi_tx/src/main.cpp
@@ -1,7 +1,23 @@
-#include "EspNowTransmitter.hpp"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_timer.h>
+#include <cstdio>
+#include <CSIFallGuard.h>
+
+// MAC của board TX (đã xác định trước)
+uint8_t tx_mac[] = {0x80, 0xB5, 0x4E, 0xDE, 0xEE, 0x70};
+uint8_t rx_mac[] = {0x80, 0xB5, 0x4E, 0xDE, 0xEC, 0x48};
 
 extern "C" void app_main() {
-    static EspNowTransmitter tx;
-    tx.begin();
+    // Set MAC trước khi begin (quan trọng!)
+    CSIFallGuard_set_tx_mac(tx_mac);
+    CSIFallGuard_set_rx_mac(rx_mac);
     
+    // Khởi tạo TX mode
+    CSIFallGuard_begin(MODE_TX, 1);
+    
+    while (1) {
+        CSIFallGuard_update();
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
 }
