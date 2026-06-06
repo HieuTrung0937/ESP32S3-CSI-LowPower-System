b01b101be40c476f1f4a4eee362cc957f11a97a5
HieuTrung0937 <ngotsacombank@gmail.com>
2026-06-07 00:07:26 +0700

    clean code cho thu vien

--- esp32s3_csi_rx/src/main.cpp
+++ esp32s3_csi_rx/src/main.cpp
@@ -1,22 +1,32 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include <esp_timer.h>
-#include <cstdio>
 #include <CSIFallGuard.h>
-// MAC thật của 2 board
-uint8_t my_tx_mac[] = {0x80, 0xB5, 0x4E, 0xDE, 0xEE, 0x70};
-uint8_t my_rx_mac[] = {0x80, 0xB5, 0x4E, 0xDE, 0xEC, 0x48};
+
+uint8_t tx_mac[] = {0x80, 0xB5, 0x4E, 0xDE, 0xEE, 0x70};
+uint8_t rx_mac[] = {0x80, 0xB5, 0x4E, 0xDE, 0xEC, 0x48};
 
 extern "C" void app_main() {
-    // Cấu hình MAC trước khi begin
-    CSIFallGuard_set_tx_mac(my_tx_mac);
-    CSIFallGuard_set_rx_mac(my_rx_mac);
+    // Cấu hình cơ bản
+    CSIFallGuard_set_tx_mac(tx_mac);
+    CSIFallGuard_set_rx_mac(rx_mac);
     CSIFallGuard_set_channel(1);
     
+    // Cấu hình chế độ in (đã có macro)
+    CSIFallGuard_set_print_mode(PRINT_MODE_RSSI | PRINT_MODE_STATE);
+    
+    // Cấu hình phát hiện nằm
+    CSIFallGuard_set_lying_params(12, 3000, 2000);
+    
+    // Khởi tạo RX
     CSIFallGuard_begin(MODE_RX, 1);
     
     while (1) {
         CSIFallGuard_update();
         vTaskDelay(pdMS_TO_TICKS(10));
+        
+        // Kiểm tra trạng thái nằm
+        if (CSIFallGuard_is_lying()) {
+            // Xử lý khi phát hiện nằm
+        }
     }
 }
--- esp32s3_csi_tx/src/main.cpp
+++ esp32s3_csi_tx/src/main.cpp
@@ -4,17 +4,16 @@
 #include <cstdio>
 #include <CSIFallGuard.h>
 
-// MAC của board TX (đã xác định trước)
 uint8_t tx_mac[] = {0x80, 0xB5, 0x4E, 0xDE, 0xEE, 0x70};
 uint8_t rx_mac[] = {0x80, 0xB5, 0x4E, 0xDE, 0xEC, 0x48};
 
 extern "C" void app_main() {
-    // Set MAC trước khi begin (quan trọng!)
+    // Cấu hình bắt buộc (cả 3 dòng)
     CSIFallGuard_set_tx_mac(tx_mac);
     CSIFallGuard_set_rx_mac(rx_mac);
-    
-    // Khởi tạo TX mode
-    CSIFallGuard_begin(MODE_TX, 1);
+    CSIFallGuard_set_channel(1);          // dù mặc định là 1, vẫn nên set
+
+    CSIFallGuard_begin(MODE_TX, 1);       // mode TX
     
     while (1) {
         CSIFallGuard_update();
